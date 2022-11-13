02543322f58e157ab997c8f3da95edd1ee888edd
GonzaloAxelH <gonzaloaxelh@gmail.com>
2022-11-12 21:06:24 -0500

    as

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,19 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-     path('', TemplateView.as_view(template_name='index.html')),
+     
     path('admin/', admin.site.urls),
 
 ] 
 
+urlpatterns += [re_path(r'^.*',
+                        TemplateView.as_view(template_name='index.html'))]
 
 
 if settings.DEBUG:
