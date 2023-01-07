54bd74a3bfc9992433eae424b17d136e984ebd82
GonzaloAxelH <gonzaloaxelh@gmail.com>
2023-01-06 21:15:38 -0500

    test static files

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,19 @@
 
 from django.contrib import admin
-from django.urls import path,re_path
+from django.urls import path, re_path
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-     
+
     path('admin/', admin.site.urls),
 
-] 
+]
 
 urlpatterns += [re_path(r'^.*',
                         TemplateView.as_view(template_name='index.html'))]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
